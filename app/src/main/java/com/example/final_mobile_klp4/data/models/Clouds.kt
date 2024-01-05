9957de2f2c48f5205e2caaadda14bc54a3e101a0
murniputri01 <murniputritamrin@gmail.com>
2024-01-05 16:55:12 +0800

    Menambahkan models API

---
+++ app/src/main/java/com/example/final_mobile_klp4/data/models/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.final_mobile.data.models
+
+data class Clouds(
+    val all: Int
+)
---
+++ app/src/main/java/com/example/final_mobile_klp4/data/models/Coord.kt
@@ -0,0 +1,6 @@
+package com.example.final_mobile.data.models
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
