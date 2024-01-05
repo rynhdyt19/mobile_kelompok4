174531552fc3c74792e049385055854e836df7d0
rynhdyt19 <104614030+rynhdyt19@users.noreply.github.com>
2024-01-05 15:34:59 +0800

    Struktur Awal Aplikasi

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FInal_Mobile_klp4"
+include(":app")
+ 
