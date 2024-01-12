dd94695e5bfcdb19563b10ff8074efd9f350d752
rynhdyt19 <104614030+rynhdyt19@users.noreply.github.com>
2024-01-12 16:19:22 +0800

    membuat function getPollution untuk mengambil data perkiraan dari API

--- app/src/main/java/com/example/final_mobile_klp4/MainActivity.kt
+++ app/src/main/java/com/example/final_mobile_klp4/MainActivity.kt
@@ -273,3 +273,65 @@ private fun getCurrentWeather(city: String) {
         }
     }
 }
+
+@OptIn(DelicateCoroutinesApi::class)
+private fun getPollution(lat: Double, lon: Double) {
+    lifecycleScope.launch(Dispatchers.IO) {
+        val response = try {
+            RetrofitInstance.api.getPollution(
+                lat,
+                lon,
+                "metric",
+                applicationContext.getString(R.string.api_key)
+            )
+        } catch (e: IOException) {
+            Toast.makeText(applicationContext, "app error ${e.message}", Toast.LENGTH_SHORT)
+                .show()
+            return@launch
+        } catch (e: HttpException) {
+            Toast.makeText(applicationContext, "http error ${e.message}", Toast.LENGTH_SHORT)
+                .show()
+            return@launch
+        }
+
+        if (response.isSuccessful && response.body() != null) {
+            withContext(Dispatchers.Main) {
+
+                val data = response.body()!!
+
+                val num = data.list[0].main.aqi
+
+                binding.tvAirQual.text = when (num) {
+                    1 -> getString(R.string.good)
+                    2 -> getString(R.string.fair)
+                    3 -> getString(R.string.moderate)
+                    4 -> getString(R.string.poor)
+                    5 -> getString(R.string.very_poor)
+                    else -> "no data"
+                }
+                binding.layoutPollution.setOnClickListener {
+                    val bundle = Bundle()
+                    bundle.putDouble("co",data.list[0].components.co)
+                    bundle.putDouble("nh3",data.list[0].components.nh3)
+                    bundle.putDouble("no",data.list[0].components.no)
+                    bundle.putDouble("no2",data.list[0].components.no2)
+                    bundle.putDouble("o3",data.list[0].components.o3)
+                    bundle.putDouble("pm10",data.list[0].components.pm10)
+                    bundle.putDouble("pm2_5",data.list[0].components.pm2_5)
+                    bundle.putDouble("so2",data.list[0].components.so2)
+
+                    pollutionFragment.arguments = bundle
+
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.frameLayout, pollutionFragment)
+                            .addToBackStack(null)
+                            .commit()
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
